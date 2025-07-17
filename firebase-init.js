0805fa2c9bf5210db656e321f6e84d87b8041990
StefanVaskovic-SD <stefan.vaskovic@studiodirection.com>
2025-07-17 11:00:45 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,8 +1,6 @@
 // firebase-init.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDytFA6jSmRXpnwiIY6hz6u0F62y5JtFA",
   authDomain: "podclub-bdcc9.firebaseapp.com",
@@ -13,10 +11,9 @@ const firebaseConfig = {
   appId: "1:980388381441:web:e12821b7b00cdf0be3d100"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+// Initialize Firebase (non-module style)
+firebase.initializeApp(firebaseConfig);
 
-window.firebaseApp = app;
-window.firebaseAuth = auth;
-window.firebaseDB = db;
+// Expose Auth and Firestore globally
+window.firebaseAuth = firebase.auth();
+window.firebaseDB = firebase.firestore();
